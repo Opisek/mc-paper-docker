297f475fbe7323eac030dc409961c08cf1c8c853
Kacper Darowski <40141286+Opisek@users.noreply.github.com>
2023-08-13 16:11:12 +0200

    fixed typo

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -15,7 +15,7 @@ export function getEnvironmental(): Environmental {
     eula: process.env.EULA === "true",
     version: process.env.VERSION || "latest",
     channel: process.env.CHANNEL || "default",
-    xms: process.env.XMX || "4G",
+    xms: process.env.XMS || "4G",
     xmx: process.env.XMX || "4G",
     gracePeriod: Number.parseInt(process.env.GRACE) || 180,
   };
